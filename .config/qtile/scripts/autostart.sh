ece08087a2c628dfbb3871200bc489a9f521a33d
Marcos Carrera <ramoscarrer@gmail.com>
2022-07-27 11:30:39 -0400

    removed unneded stuff for ubuntu

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -16,12 +16,7 @@ fi
 #starting utility applications at boot time
 run nm-applet &
 run xfce4-power-manager &
-numlockx on &
-blueberry-tray &
-picom --glx-no-stencil --glx-no-rebind-pixmap --config $HOME/.config/qtile/scripts/picom.conf &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/xfce4/notifyd/xfce4-notifyd &
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 
 #starting user applications at boot time
-run volumeicon &
 run cbatticon &
