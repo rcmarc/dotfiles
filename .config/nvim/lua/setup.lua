7b9821b84ff1be3faa5a670ac93419e70f7ccc3a
Marcos Carrera <ramoscarrer@gmail.com>
2023-04-25 13:20:18 -0400

    fixed omnisharp

---
+++ .config/nvim/lua/setup.lua
@@ -0,0 +1,10 @@
+require('trouble').setup {}
+require('toggleterm').setup {}
+require('telescope').setup {}
+require('nvim-tree').setup {}
+require('nvim-autopairs').setup {}
+require('luasnip.loaders.from_vscode').lazy_load {}
+require('lualine').setup {}
+require('lspsaga').setup {}
+require('Comment').setup {}
+require('bufferline').setup {}
