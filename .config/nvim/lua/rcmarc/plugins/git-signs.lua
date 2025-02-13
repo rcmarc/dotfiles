ccfa074b9efacacfaff2f3aab7dd24f498d4be4a
Marcos Carrera <mcramos97@yahoo.com>
2025-02-13 15:01:04 -0500

    fix: yet another typo

--- .config/nvim/lua/rcmarc/plugins/git-signs.lua
+++ .config/nvim/lua/rcmarc/plugins/git-signs.lua
@@ -2,7 +2,7 @@ return {
 	"lewis6991/gitsigns.nvim",
 	opts = {},
 	init = function()
-		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_chunk<CR>")
+		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
 		vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>")
 	end
 }
