1fe1a5b29d38c4ed97b26614128c98af03a91f46
Marcos Carrera <marcos.crs@yahoo.com>
2026-01-13 12:57:21 -0500

    dont track the lsp enable file

--- .config/nvim/lua/rcmarc/lsp/enable.lua
+++
@@ -1,7 +0,0 @@
--- lua file for just enabling configs
-vim.lsp.enable('eslint')
-vim.lsp.enable('gopls')
-vim.lsp.enable('pyright')
-vim.lsp.enable('tailwindcss')
-vim.lsp.enable('omnisharp')
-vim.lsp.enable('jsonls')
