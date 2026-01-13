a2dd87d2fe37c061a6dc0f42070757b97373985c
Marcos Carrera <marcos.crs@yahoo.com>
2026-01-13 10:40:35 -0500

    removed vue

--- .config/nvim/lua/rcmarc/lsp/typescript.lua
+++ .config/nvim/lua/rcmarc/lsp/typescript.lua
@@ -1,15 +1,4 @@
-local NODE_HOME = os.getenv('NODE_HOME')
-
 vim.lsp.config('ts_ls', {
-	init_options = {
-		plugins = {
-			{
-				name = "@vue/typescript-plugin",
-				location = NODE_HOME .. '/lib/node_modules/@vue/typescript-plugin',
-				languages = { "javascript", "typescript", "vue" },
-			},
-		},
-	},
 	filetypes = {
 		"javascript",
 		"javascriptreact",
