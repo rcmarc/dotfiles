e7bf76ac47fd842374cafdece2637d56f33f3664
Marcos Carrera <marcos.crs@yahoo.com>
2026-02-17 11:18:32 -0500

    change telescope branch to fix #3487

--- .config/nvim/lua/rcmarc/plugins/telescope.lua
+++ .config/nvim/lua/rcmarc/plugins/telescope.lua
@@ -1,7 +1,10 @@
 return {
 	'nvim-telescope/telescope.nvim',
-	tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	version = '*',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'nvim-telescope/telescope-fzf-native.nvim', build = 'make'
+	},
 	opts = function()
 		local builtin = require('telescope.builtin')
 
