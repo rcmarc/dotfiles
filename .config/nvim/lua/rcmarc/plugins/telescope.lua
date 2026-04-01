a6d8a599c1bf85d451112c2483f2ea03906795a3
Marcos Carrera <marcos.crs@yahoo.com>
2026-04-01 13:24:42 -0400

    updated packages and changed telescope config

--- .config/nvim/lua/rcmarc/plugins/telescope.lua
+++ .config/nvim/lua/rcmarc/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
 	version = '*',
 	dependencies = {
 		'nvim-lua/plenary.nvim',
-		'nvim-telescope/telescope-fzf-native.nvim', build = 'make'
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
 	},
 	opts = function()
 		local builtin = require('telescope.builtin')
@@ -11,7 +11,7 @@ return {
 		vim.keymap.set('n', '<leader>ff', function() builtin.find_files { hidden = true } end,
 			{ desc = 'Telescope find files' })
 		vim.keymap.set('n', '<leader>fgt', builtin.git_files, { desc = 'Telescope find files' })
-		vim.keymap.set('n', '<leader>fgr', builtin.live_grep, { desc = 'Telescope live grep' })
+		vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = 'Telescope live grep' })
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 	end
